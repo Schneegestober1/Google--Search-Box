b73a099a52a9824d2bc18f2691c2e8ea0f4a1a6b
Kholov Rustam <rustamkholow5@gmail.com>
2024-12-24 19:29:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let searchBox = document.getElementById('searchBox')
+let googleIcon = document.getElementById('googleIcon')
+
+googleIcon.onclick = function(){
+    searchBox.classList.toggle('active')
+}
